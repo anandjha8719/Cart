ec5b243d4f2567d36a0c34d79bfb879977b4ac83
anandjha8719 <anandjha8719@gmail.com>
2021-11-24 13:07:23 +0530

    props + increase qty

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,59 @@ import react from "react";
 import CartItem from "./CartItem";
 
 class Cart extends react.Component {
+    constructor(){
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 999,
+                    title: 'iphone',
+                    qty: 2,
+                    img: '',
+                    id: 1
+                },
+                {
+                    price: 9,
+                    title: 'watch',
+                    qty: 1,
+                    img: '',
+                    id:2
+                },
+                {
+                    price: 909,
+                    title: 'Ipad',
+                    qty: 3,
+                    img: '',
+                    id: 3
+                }
+            ]
+        }
+    }
+
+    handleIncreaseQuantity = (product) => {
+        const { products } = this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+        
+        this.setState({
+            products: products
+        })
+    }
+
     render (){
+        const {products} = this.state;
         return (
             <div className="cart">
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                {products.map((product) => {
+                    return (
+                        <CartItem 
+                            product={product}
+                            key={product.id}
+                            onIncreaseQuantity={this.handleIncreaseQuantity}
+                        />
+                    )
+                })}
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,42 +2,9 @@ import react from 'react';
 import React from 'react';
 
 class CartItem extends react.Component {
-    constructor(){
-        super();
-        this.state = {
-            price: 999,
-            title: 'iphone',
-            qty: 1,
-            img: ''
-        }
-    }
-    increaseQuantity = () => {
-        //object form of thisState func
-        // this.setState({
-        //     qty: this.state.qty + 1
-        // });
-
-        //function form of thisState function, use when prevstate is required
-        this.setState((prevState) =>{
-            return {
-                qty: prevState.qty + 1
-            }
-        });
-    }
-    decreaseQuantity = () => {
-        const { qty } = this.state;
-        if(qty == 0) {
-            return;
-        }
-        this.setState((prevState) =>{
-            return {
-                qty: prevState.qty - 1
-            }
-        });
-    }
 
     render() {
-        const { price, title, qty } = this.state;
+        const { price, title, qty } = this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -45,11 +12,11 @@ class CartItem extends react.Component {
                 </div>
                 <div className="right-block">
                     <div style={ { fontSize: 25 } }>{title}</div>
-                    <div style={ { color: '#777' } }>Rs {price}</div>
+                    <div style={ { color: '#777' } }>Rs. {price}</div>
                     <div style={ { color: '#777' } }>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         <img style={styles.button} alt="increase" className="action-actions" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
-                            onClick={this.increaseQuantity}
+                            onClick={() => this.props.onIncreaseQuantity(this.props.product)}
                         />
                         <img style={styles.button} alt="decrease" className="action-actions" src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
                             onClick={this.decreaseQuantity}
